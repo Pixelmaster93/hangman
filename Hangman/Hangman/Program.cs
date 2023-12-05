8b57d011a89c12037f300f796b5c16a46f23b9a8
Pixelmaster <andy.toro93@gmail.com>
2023-12-05 11:40:58 +0100

    Hangman 1.0

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -4,6 +4,7 @@ namespace Hangman
 {
     internal class Program
     {
+        //void si usa quando non ci si apetta nulla di ritorno, se non c'e void alla fine ci sarà un return
         static void Main(string[] args)
         {
             //array=lista di parole da prendere
@@ -22,13 +23,107 @@ namespace Hangman
             int lives = 3;
 
             // Lista di lettere provate dal player. List è come un array ma variabile ovvero uyn alist adi oggetti variabili
-            List<string> list = new();
+            List<string> letters = new();
 
             //Fino a quando il player ha vite il gioco continua. While continua a girare fino a quando la condizione non è vera
             while (lives != 0)
             {
+                //contatore delle lettere che devono ancora essere indovinate
+                int charactersLeft = 0;
+                //Serve a mettere i trattini in base alle letetre della parola da indovinare
+                //loop tra tutti i caratteri della parola scelta
+                foreach (var character in wordToGuess)
+                {
+                    //trasforma qualsiasi variabile in stringa in questo caso character
+                    var letter = character.ToString();
 
+                    //se quel contenitore contiene la nostra lettera = Contains
+                    if (letters.Contains(letter))
+                    {
+                        Console.Write(letter);
+                    }
+
+                    //altriemnti
+                    else
+                    {
+                        //write non va a capo
+                        Console.Write("_");
+
+                        //incrementa di 1 le lettere da indovinare
+                        charactersLeft++;
+
+                    }
+                }
+
+                Console.WriteLine(String.Empty);
+
+                if (charactersLeft == 0)
+                {
+                    break;
+                }
+
+                Console.Write("Type in a letter: ");
+
+                //aspetta la pressione da paret dell'utente di un tasto
+
+                //var consoleKey = Console.ReadKey();
+
+                //var key = consoleKey.Key;
+
+                //var ourLetter = key.ToString();
+
+                var key = Console.ReadKey().Key.ToString().ToLower(); //Più semplificato del metodo sopra. ToLower serve a trasformare in minuscola la lettera
+
+                Console.WriteLine(String.Empty);
+
+                //Il ! inverte il valore dell'espressione, ovvero in questo caso se il carattere è diverso da quello possibile
+                if (!validCharacters.IsMatch(key))
+                {
+                    Console.WriteLine($"The letetr {key} is invalid. Try again.");
+                    continue;
+                }
+                if (letters.Contains(key))
+                {
+                    Console.WriteLine("You have already entered this letter!");
+                    continue;
+                }
+
+                //Add aggiunge il tasto all'elenco dei tasti premuti
+                letters.Add(key);
+
+                //se la letterea non è corretta diminuisco di 1 la vita e scrivo qualcosa
+                if (!wordToGuess.Contains(key))
+                {
+                    lives--;
+
+                    var placeholder = lives == 1 ? "life" : "lives";
+
+                    if (lives > 0)
+                    {
+                        Console.WriteLine($"The letter {key} is not in the word to guess. You have {lives} {placeholder} left");
+                    }
+
+                    //Metodo più lungo di quello sopra
+                    //if (lives > 1)
+                    //{
+                    //    Console.WriteLine($"The letter {key} is not in the word to guess. You have {lives} lives left");
+                    //}
+                    //else if (lives == 1)
+                    //{
+                    //    Console.WriteLine($"The letter {key} is not in the word to guess. You have 1 life left");
+                    //}
+                }
+
+                if (lives > 0)
+                {
+                    Console.WriteLine($" You have {lives} lives left");
+                }
+                else
+                {
+                    Console.WriteLine($" You have lost! The word was {wordToGuess}.");
+                }
             }
+
         }
     }
 }
